4f6ea48d8575da672fe3b8367bae2b5f404c7237
soonland <76574139+soonland@users.noreply.github.com>
2024-04-10 21:45:17 -0400

    feat: search by tracks

--- models/types.ts
+++ models/types.ts
@@ -16,6 +16,41 @@ export interface IArtist {
   url: string;
 }
 
+export interface IAlbum {
+  album_type: string;
+  artists: IArtist[];
+  available_markets: string[];
+  external_urls: { spotify: string };
+  href: string;
+  id: string;
+  images: IImage[];
+  name: string;
+  release_date: string;
+  release_date_precision: string;
+  total_tracks: number;
+  type: string;
+  uri: string;
+}
+
+export interface ITracks {
+  album: IAlbum;
+  artists: IArtist[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: { isrc: string };
+  external_urls: { spotify: string };
+  href: string;
+  id: string;
+  is_local: boolean;
+  name: string;
+  popularity: number;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
+}
+
 export interface ISearchArtists {
   href: string;
   items: IArtist[];
@@ -26,6 +61,17 @@ export interface ISearchArtists {
   total: number;
 }
 
+export interface ISearchTracks {
+  href: string;
+  items: ITracks[];
+  limit: number;
+  next: string;
+  offset: number;
+  previous: string;
+  total: number;
+}
+
 export interface ISearch {
   artists: ISearchArtists;
+  tracks: ISearchTracks;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,36 @@
 import { useSession } from "next-auth/react";
-import { Box, CircularProgress, Grid, styled } from "@mui/material";
+import { Box, Grid, styled } from "@mui/material";
 import Image from "next/image";
 import useSWRMutation from "swr/mutation";
 import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
 import useTranslation from "next-translate/useTranslation";
 import { useRouter } from "next/router";
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
 
 const StyledDataGrid = styled(DataGrid)(({ theme }) => ({
   "& .MuiDataGrid-columnHeaders": {
     backgroundColor: theme.palette.mode === "light" ? "#fafafa" : "#1d1d1d",
   },
 }));
+
+const StyledGridOverlay = styled("div")(() => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100%",
+}));
+
+const CustomNoRowsOverlay = () => {
+  return (
+    <StyledGridOverlay>
+      <SentimentVeryDissatisfiedIcon />
+      <Box sx={{ mt: 1 }}>No Rows</Box>
+    </StyledGridOverlay>
+  );
+};
+
 const Home = () => {
   const session = useSession();
   const router = useRouter();
@@ -75,25 +94,27 @@ const Home = () => {
           <Grid container flexDirection={"column"} spacing={2}>
             {session.status === "authenticated" && (
               <Grid item>
-                {isMutating && <CircularProgress />}
-                {!isMutating && (
-                  <StyledDataGrid
-                    rowHeight={80}
-                    pageSizeOptions={[5, 10, 25]}
-                    pagination
-                    paginationModel={paginationModel}
-                    onPaginationModelChange={setPaginationModel}
-                    columns={columns}
-                    rows={
-                      data?.artists?.items?.map((el, index) => ({
-                        id: index,
-                        artistName: el.name,
-                        albumCover: el.images[2]?.url,
-                        spotifyLink: el.external_urls.spotify,
-                      })) || []
-                    }
-                  />
-                )}
+                <StyledDataGrid
+                  autoHeight
+                  rowHeight={80}
+                  checkboxSelection
+                  pageSizeOptions={[5, 10, 25]}
+                  pagination
+                  loading={isMutating}
+                  paginationModel={paginationModel}
+                  onPaginationModelChange={setPaginationModel}
+                  columns={columns}
+                  rows={
+                    data?.artists?.items?.map((el, index) => ({
+                      id: index,
+                      artistName: el.name,
+                      albumCover: el.images[2]?.url,
+                      spotifyLink: el.external_urls.spotify,
+                    })) || []
+                  }
+                  slots={{ noRowsOverlay: CustomNoRowsOverlay }}
+                  sx={{ "--DataGrid-overlayHeight": "100px" }}
+                />
               </Grid>
             )}
           </Grid>
