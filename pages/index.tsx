37b5d0b4741c7b3e07a7675c065a670ee1e65e80
soonland <76574139+soonland@users.noreply.github.com>
2024-05-04 20:08:24 -0400

    refactor: search grid component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,15 @@
 import { useSession } from "next-auth/react";
-import { Box, Grid, Link, styled } from "@mui/material";
-import Image from "next/image";
+import { Box, Grid } from "@mui/material";
 import useSWRMutation from "swr/mutation";
-import { GridAlignment, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
-import { useEffect, useState } from "react";
-import useTranslation from "next-translate/useTranslation";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
-import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
 import { ParsedUrlQuery } from "querystring";
+import SearchDataGrid from "@/components/Grid/SearchDataGrid";
 import { ISearch } from "@/models/types";
-import EnhancedDataGrid from "@/components/EnhancedDataGrid";
-
-const StyledGridOverlay = styled("div")(() => ({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  height: "100%",
-}));
-
-const CustomNoRowsOverlay = () => {
-  const { t } = useTranslation("common");
-  return (
-    <StyledGridOverlay>
-      <SentimentVeryDissatisfiedIcon fontSize="large" />
-      <Box sx={{ mt: 1 }}>{t("common.noDataFound")}</Box>
-    </StyledGridOverlay>
-  );
-};
 
 const Home = () => {
   const session = useSession();
   const router = useRouter();
-  const { t } = useTranslation("common");
 
   const fetcher = async (url: string, { arg }: { arg: { searchType: string; searchString: string } }) => {
     if (arg.searchType === undefined) {
@@ -49,56 +26,6 @@ const Home = () => {
 
   const { data, isMutating, trigger } = useSWRMutation("/api/search", fetcher);
 
-  const [paginationModel, setPaginationModel] = useState({
-    pageSize: 5,
-    page: 0,
-  });
-
-  const createColumn = (
-    field: string,
-    headerName: string,
-    width: number,
-    align: GridAlignment | undefined = "center",
-  ): GridColDef => {
-    return {
-      field,
-      headerName: t(`dataGrid.search.${field}`),
-      width,
-      headerAlign: align,
-      align,
-    };
-  };
-
-  const columns = [
-    createColumn("type", "type", 100),
-    createColumn("artistName", "artistName", 150),
-    createColumn("albumName", "albumName", 150),
-    createColumn("trackName", "trackName", 150),
-    {
-      ...createColumn("imgCover", "imgCover", 150),
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      renderCell: (params: GridRenderCellParams<any, string>) => (
-        <Image src={params.value as string} alt="" width={80} height={80} />
-      ),
-    },
-    {
-      ...createColumn("spotifyLink", "spotifyLink", 150),
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      renderCell: (params: GridRenderCellParams<any, string>) => (
-        <a href={params.value as string} target="_blank" rel="noopener noreferrer">
-          Click Me
-        </a>
-      ),
-    },
-    {
-      ...createColumn("recommandationLink", "recommandationLink", 150),
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      renderCell: (params: GridRenderCellParams<any, string>) => {
-        return <Link href={`/recommendations/${params.row.type}/${params.row.id}`}>Click Me</Link>;
-      },
-    },
-  ];
-
   const handleSearch = (searchQuery: ParsedUrlQuery) => {
     const searchString = searchQuery.q as string;
     const searchType = searchQuery.type as string;
@@ -113,6 +40,7 @@ const Home = () => {
   }, [router.query.q, router.query.type]);
 
   const convertResultsToDataGridRows = (data: ISearch) => {
+    console.log("data", data);
     const rows =
       data?.artists?.items?.map((artist) => ({
         type: artist.type,
@@ -146,6 +74,9 @@ const Home = () => {
         spotifyLink: track.external_urls.spotify,
       })) || [];
     // concat all rows
+    console.log("rows", rows);
+    console.log("rows2", rows2);
+    console.log("rows3", rows3);
     return rows.concat(rows2).concat(rows3);
   };
 
@@ -156,19 +87,7 @@ const Home = () => {
           <Grid container flexDirection={"column"} spacing={2}>
             {session.status === "authenticated" && (
               <Grid item>
-                <EnhancedDataGrid
-                  autoHeight
-                  rowHeight={80}
-                  pageSizeOptions={[5, 10, 25, 50, 100]}
-                  pagination
-                  loading={isMutating}
-                  paginationModel={paginationModel}
-                  onPaginationModelChange={setPaginationModel}
-                  columns={columns}
-                  rows={convertResultsToDataGridRows(data)} // Ensure data is not undefined
-                  slots={{ noRowsOverlay: CustomNoRowsOverlay }}
-                  sx={{ "--DataGrid-overlayHeight": "100px" }}
-                />
+                <SearchDataGrid isMutating={isMutating} data={convertResultsToDataGridRows(data)} />
               </Grid>
             )}
           </Grid>
